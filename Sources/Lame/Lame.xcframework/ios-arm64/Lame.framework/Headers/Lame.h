49aac92b39a2adb245d4a31fec37de3d11c76aa5
VeryLoveLoli <chuancan@icloud.com>
2021-03-05 17:41:01 +0800

    修复 其它xcframework的.a库 也是使用 module.modulemap 引起的冲突  改成 .framework库

---
+++ Sources/Lame/Lame.xcframework/ios-arm64/Lame.framework/Headers/Lame.h
@@ -0,0 +1,18 @@
+//
+//  Lame.h
+//  Lame
+//
+//  Created by 韦烽传 on 2021/3/5.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Lame.
+FOUNDATION_EXPORT double LameVersionNumber;
+
+//! Project version string for Lame.
+FOUNDATION_EXPORT const unsigned char LameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lame/PublicHeader.h>
+
+#import <Lame/lame_c.h>
